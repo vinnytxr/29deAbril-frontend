4d09aa480f8067ed1a3f9a93cbd02b89657f7a83
comit teste <christofer_daniel12@hotmail.com>
2023-04-07 09:42:32 -0300

    Começar a partir daqui. Projeto limpo depois de testes iniciais com o hosting do firebase

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client"
-
-// app
-import App from "./App"
-import LoginScreen from "./LoginScreen"
-import RegisterScreen from "./RegisterScreen"
-
-const root = document.querySelector("#root")
-ReactDOM.createRoot(root).render(<RegisterScreen />)
