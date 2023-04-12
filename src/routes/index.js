afe2ae71d19aa09dbc4a7fd43e76749d0dfb18c5
ChristoferLv <christofer_daniel12@hotmail.com>
2023-04-12 19:22:31 -0300

    Adicionada home screen onde são mostrados todos os cursos na plataforma

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,15 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/Login";
 import RegisterScreen from "../pages/Register";
 import NewCourseScreen from "../pages/CreateCourse";
+import Home from "../pages/Home/Home";
 
 const Routes = createBrowserRouter([
   {
     path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
     element: <LoginPage/>,
   },
   {
@@ -17,6 +22,7 @@ const Routes = createBrowserRouter([
     path: "/novocurso",
     element: <NewCourseScreen/>,
   },
+  
 ]);
 
 export default Routes
