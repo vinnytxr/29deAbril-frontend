c56965beb26ebced13cd561a6f44419b94acfe0b
ChristoferLv <christofer_daniel12@hotmail.com>
2023-04-12 11:01:00 -0300

    Adicionada tela de cadastro de aluno e ajustadas as rotas com a nova tela

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import LoginPage from "../pages/Login";
+import RegisterScreen from "../pages/Register";
 
 const Routes = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage/>,
+  },
+  {
+    path: "/register",
+    element: <RegisterScreen/>,
   },
 ]);
 
