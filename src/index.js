b1cccddc20a3c4134c0cb96c477b2d74c52103e6
Iago Carmona <iagoortegacarmona@gmail.com>
2023-04-13 22:16:47 -0300

    Removendo SideNav das rotas de login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-import {RouterProvider} from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 
 // app
-import Routes from "./routes"
+import DefaultRoutes from "./routes"
 
 const root = document.querySelector("#root")
 ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <RouterProvider router={Routes} />
+        <DefaultRoutes />
     </React.StrictMode>
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
+
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 
 import LoginPage from "../pages/Login";
 import RegisterScreen from "../pages/Register";
 import NewCourseScreen from "../pages/CreateCourse";
 import Home from "../pages/Home/Home";
+import SideNav from '../components/NavBar';
 
-const Routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/login",
-    element: <LoginPage/>,
-  },
-  {
-    path: "/register",
-    element: <RegisterScreen/>,
-  },
-  {
-    path: "/novocurso",
-    element: <NewCourseScreen/>,
-  },
-  
-]);
+const SidebarLayout = () => (
+  <>
+    <SideNav />
+    <Outlet />
+  </>
+);
 
-export default Routes
+const DefaultRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterScreen/>}/>
+        <Route element={<SidebarLayout/>}>
+          <Route path='/' element={<Home /> }/>
+          <Route path='/novo-curso' element={<NewCourseScreen /> }/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default DefaultRoutes
