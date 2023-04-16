51cd8d647fed33464747b58f139fc400e115e261
Iago Carmona <iagoortegacarmona@gmail.com>
2023-04-16 19:16:39 -0300

    Arrumando identação e criando pagina de perfil do usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,41 +1,45 @@
-
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
-import { Row } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom'
+import { Col, Row } from 'react-bootstrap'
 
 // Components
-import SideNav from '../components/NavBar';
+import SideNav from '../components/NavBar'
 
 // Pages
-import LoginPage from "../pages/Login";
-import RegisterScreen from "../pages/Register";
-import NewCourseScreen from "../pages/CreateCourse";
-import Home from "../pages/Home/Home";
-import UserProfileScreen from '../pages/UserProfile';
+import LoginPage from '../pages/Login'
+import RegisterScreen from '../pages/Register'
+import NewCourseScreen from '../pages/CreateCourse'
+import Home from '../pages/Home/Home'
+import UserProfileScreen from '../pages/UserProfile'
 
 const SidebarLayout = () => (
   <>
-      <SideNav />
-      <Outlet />
+    <SideNav />
+    <Row>
+      <div className="col-2" />
+      <Col>
+        <Outlet />
+      </Col>
+    </Row>
   </>
-);
+)
 
 const CSSBaseline = () => (
   <>
-      <div className="baseline"/>
+    <div className="baseline" />
   </>
 )
 
 const DefaultRoutes = () => {
   return (
     <BrowserRouter>
-    <CSSBaseline/>
+      <CSSBaseline />
       <Routes>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/register' element={<RegisterScreen/>}/>
-        <Route element={<SidebarLayout/>}>
-          <Route path='/' element={<Home /> }/>
-          <Route path='/novo-curso' element={<NewCourseScreen /> }/>
-          <Route path='/perfil' element={<UserProfileScreen /> }/>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route element={<SidebarLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/novo-curso" element={<NewCourseScreen />} />
+          <Route path="/perfil" element={<UserProfileScreen />} />
         </Route>
       </Routes>
     </BrowserRouter>
