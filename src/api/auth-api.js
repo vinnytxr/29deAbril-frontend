6c8fb49ec20bbcda222be9bb1904e06105c67e03
Iago Carmona <iagoortegacarmona@gmail.com>
2023-04-24 22:26:00 -0300

    #6 - Criando função para pegar os cursos do professor

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -76,9 +76,36 @@ const getCoursesData = async (jwt) => {
     }
 }
 
+const getProfessorCourses = async(professorId, page = 1, size = 10) => {
+    const url = `${BASE_URL}/courses/courses?page=${page}&size=${size}&professor=${professorId}`
+    try {
+        const options = {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                // jwt: jwt,
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            }
+        }
+
+        const response = await fetch(url, options);
+
+        if (response.ok) {
+            const data = await response.json();
+            AUTH_DEBUG && console.log("AuthAPI::getProfessorCourses(): ", data);
+            return new HttpResponse(HttpStatus.OK, data);
+        } else throw new Error("Error on getProfessorCourses()");
+    } catch (error) {
+        console.warn(error)
+        return new HttpResponse(HttpStatus.ERROR, null);
+    }
+}
+
 
 export const AuthAPI = {
     getUserInfo,
     login,
-    getCoursesData
+    getCoursesData,
+    getProfessorCourses,
 }
