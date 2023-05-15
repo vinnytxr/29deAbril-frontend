dadf3c2e5b64538ef9094fbd9eff6f03bab218a1
ChristoferLv <christofer_daniel12@hotmail.com>
2023-05-15 17:01:58 -0300

    Ajustadas as listagens quebrarem quando não tinha nada para listar

--- src/pages/StudentCourses/student_courses.jsx
+++ src/pages/StudentCourses/student_courses.jsx
@@ -2,30 +2,32 @@
 import React, { useEffect, useState } from 'react'
 
 import { Col, Container, Navbar, Row, Pagination } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthAPI } from '../../api/auth-api'
 import { HttpStatus } from '../../api/default'
 import { useAuthContext } from '../../contexts/AuthContext';
 import CardCourses from '../../components/CardCourses'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPointLeft } from '@fortawesome/free-solid-svg-icons'
 
 const StudentCoursesPage = () => {
     const [userData, setUserData] = useState({ name: "" });
     const [data, setData] = useState({});
     const [isFetched, setIsFetched] = useState(false);
-    const { logged, user } = useAuthContext();
     const [activePage, setActivePage] = useState(1);
     const [amountPages, setAmountPages] = useState(6);
+    const navigate = useNavigate();
 
     const pageSize = 6;
 
     const renderPagination = () => {
         let items = []
-        for(let i = 1; i <= amountPages; i++){
-            if(!isFetched){
+        for (let i = 1; i <= amountPages; i++) {
+            if (!isFetched) {
                 items.push(
                     <Pagination.Item key={i} active={i === activePage} disabled onClick={() => setActivePage(i)}>{i}</Pagination.Item>
                 )
-            }else {
+            } else {
                 items.push(
                     <Pagination.Item key={i} active={i === activePage} onClick={() => setActivePage(i)}>{i}</Pagination.Item>
                 )
@@ -37,13 +39,13 @@ const StudentCoursesPage = () => {
                 <Pagination>
                     {amountPages > 1 && (
                         <>
-                            {isFetched ? 
-                                <Pagination.Prev onClick={() => activePage > 1 && setActivePage((prevState) => prevState - 1)}/>
-                                : <Pagination.Prev disabled onClick={() => activePage > 1 && setActivePage((prevState) => prevState - 1)}/>
+                            {isFetched ?
+                                <Pagination.Prev onClick={() => activePage > 1 && setActivePage((prevState) => prevState - 1)} />
+                                : <Pagination.Prev disabled onClick={() => activePage > 1 && setActivePage((prevState) => prevState - 1)} />
                             }
                             {items}
-                            {isFetched ? <Pagination.Next onClick={() => activePage < 3 && setActivePage((prevState) => prevState + 1)}/>
-                            : <Pagination.Next disabled onClick={() => activePage < 3 && setActivePage((prevState) => prevState + 1)}/>}      
+                            {isFetched ? <Pagination.Next onClick={() => activePage < 3 && setActivePage((prevState) => prevState + 1)} />
+                                : <Pagination.Next disabled onClick={() => activePage < 3 && setActivePage((prevState) => prevState + 1)} />}
                         </>
                     )}
                 </Pagination>
@@ -61,7 +63,7 @@ const StudentCoursesPage = () => {
         const getStudentCourses = async () => {
             setIsFetched(false)
             try {
-                const {id} = userData;
+                const { id } = userData;
                 const responseCourses = await AuthAPI.getStudentCourses(id, activePage, pageSize)
                 if (responseCourses.status === HttpStatus.OK) {
                     setData(responseCourses.data)
@@ -69,7 +71,7 @@ const StudentCoursesPage = () => {
                     setAmountPages(pages)
                     setIsFetched(true)
                 }
-            } catch(err){
+            } catch (err) {
                 console.log(err)
             }
         }
@@ -80,10 +82,11 @@ const StudentCoursesPage = () => {
     return (
         <Row>
             <Row className="home-card mt-5">
-                    <div className="col">
-                        <h1 className="mb-3 fs-5 fw-bold">Meus cursos</h1>
+                <div className="col">
+                    <h1 className="mb-3 fs-5 fw-bold">Meus cursos</h1>
 
-                        {isFetched && logged && !!user ? (
+                    {isFetched ? (
+                        data.results.length ? (
                             <Row className="g-4">
                                 {data.results.map((course) => (
                                     <Col xs={12} lg={4} key={course.id}>
@@ -93,9 +96,19 @@ const StudentCoursesPage = () => {
                                     </Col>
                                 ))}
                             </Row>
-                        ) : <></>}
-                    </div>
-                </Row>
+                        ) : (
+                            <Container fluid>
+                                <div class="d-flex align-items-center justify-content-center" style={{"height": "350px"}}>
+                                    <span><h1>Parece que você ainda não se inscreveu em nenhum curso :&#41;</h1>
+                                    <p className='fs-5' style={{ color: "#1dbfb0", cursor:"pointer" }} onClick={() => {navigate('/')}}>Clique aqui para ver nossa lista de cursos <FontAwesomeIcon className='fs-5' style={{color:"yellow"}} icon={faHandPointLeft}/></p></span>
+                                </div>
+                            </Container>
+                        )
+                    ) : (
+                        <p>Carregando...</p>
+                    )}
+                </div>
+            </Row>
             <Row className='mt-5'>
                 <Col className='d-flex justify-content-center'>
                     {renderPagination()}
