55556916173b44a5d625737ee0c720afa00deae7
alessandro <git.aleneves@gmail.com>
2023-06-02 22:27:10 -0300

    #115: validação lesson obj

--- src/components/AccordionList/accordion_list.jsx
+++ src/components/AccordionList/accordion_list.jsx
@@ -6,20 +6,39 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import "./accordion_list.css";
 import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from "../../contexts/AuthContext";
+
+const isAValidLessonResume = ( obj ) => {
+  if (typeof obj !== 'object') return false;
+  if (
+    !Number.isInteger(obj.id) || 
+    typeof obj.title !== 'string' || 
+    typeof obj.banner !== 'string' || 
+    typeof obj.content !== 'string' || 
+    !Array.isArray(obj.users_who_completed)
+  ) return false;
+  if(!!obj.users_who_completed.length && !Number.isInteger(obj.users_who_completed[0]))
+    return false;
+  return true;
+}
 
 function AccordionListCourse({ sessions }) {
   const { lessons } = sessions;
+  const { user, logged } = useAuthContext();
   const navigate = useNavigate();
+
+  React.useEffect(() => {
+    console.log("lessons on lesson list: ", lessons);
+  }, [lessons])
 
   return (
     <Accordion alwaysOpen className="lesson-accordion">
-      {lessons?.map(item => (
+      {lessons?.filter(isAValidLessonResume).map(item => (
         <Card key={item.id}>
           <Accordion.Item eventKey={item.id.toString()} key={item.id}>
             <Accordion.Header className='accordion-header'>
               <section className="lesson-on-lesson-list">
                 <span>{item.title}</span>
-                <span className="completed-flag">Concluído</span>
               </section>
             </Accordion.Header>
             <Accordion.Body>
