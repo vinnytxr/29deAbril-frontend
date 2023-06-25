21627b55ec281b2ef904d01594e060fa271d5122
alessandro <git.aleneves@gmail.com>
2023-06-25 03:58:29 -0300

    #127: implementado stilos DnD categorias e menu de opções

--- src/pages/EditCategoryPage/index.js
+++ src/pages/EditCategoryPage/index.js
@@ -1,10 +1,14 @@
 import './styles.css'
 
 import { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { DndContext, closestCenter } from '@dnd-kit/core';
 import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { SortableItem } from './sortable-item';
+
+import { FaTrash } from 'react-icons/fa';
+import { IoSave } from 'react-icons/io5';
+import { FaPlus } from 'react-icons/fa';
 
 export function EditCategoryPage() {
   const [languages, setLanguages] = useState([
@@ -22,25 +26,93 @@ export function EditCategoryPage() {
   ]);
 
   return (
-    <DndContext
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
-      <Container className='p-3' style={{ 'width': '50%'}} align='center'>
-        <h3>The best programming languages</h3>
-        <SortableContext
-          items={languages}
-          strategy={verticalListSortingStrategy}
-        >
-          <section
-            style={{'height': '80vh', 'overflow-y': 'scroll', 'overflow-x': 'hidden'}}
+    <Container className='pt-3 edit-category-page'>
+      <Row>
+        <Col xs={7}>
+          <DndContext
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
           >
-            {languages.map(language => <SortableItem key={language} id={language} />)}
-          </section>
-        </SortableContext>
-      </Container>
+            <h3 className='text-center'>Categorias do curso</h3>
+            <SortableContext
+              items={languages}
+              strategy={verticalListSortingStrategy}
+              disabled={true}
+            >
+              <section
+                style={{ 'height': '80vh', 'overflow-y': 'scroll', 'overflow-x': 'scroll', 'border': '1px solid green' }}
+                className='px-5 category-dnd-container'
+              >
+                {languages.map(language => <SortableItem key={language} id={language} />)}
+              </section>
+            </SortableContext>
+          </DndContext>
+        </Col>
+        <Col xs={5}>
+          <h3 className='text-center'>Criar nova categoria</h3>
+          <div className='w-100 d-flex flex-row justify-content-between'>
+            <input
+              id='create-course-category-input'
+              type='text'
+              className='w-100 me-1 px-1'
+              style={{ 'font-size': '14px' }}
+            />
+            <Button
+              onClick={() => {
+                const inputElement = document.getElementById('create-course-category-input')
+                const inputValue = inputElement.value;
 
-    </DndContext>
+                if (inputValue) setLanguages([...languages, inputValue]);
+
+                inputElement.value = '';
+              }}
+              className='add-category-btn'
+            >
+              <FaPlus />
+            </Button>
+          </div>
+
+          <h3 className='text-center mt-5'>Opções da Categoria</h3>
+          <input
+            id='edit-course-category-input'
+            type='text'
+            className='w-100 me-1 px-1'
+            style={{ 'font-size': '14px', 'text-align':'center' }}
+            autoComplete='off'
+            spellCheck={false}
+          />
+          <div className='w-100 d-flex flex-row justify-content-between mt-2'>
+            <Button
+              onClick={() => {
+                const inputElement = document.getElementById('edit-course-category-input')
+                const inputValue = inputElement.value;
+
+                if (inputValue) setLanguages([...languages.slice(0, 3), inputValue, ...languages.slice(3+1)]);
+
+                inputElement.value = '';
+              }}
+              className='save-category-btn'
+            >
+              <IoSave />
+            </Button>
+            <Button
+              onClick={() => {
+                const inputElement = document.getElementById('edit-course-category-input')
+                const inputValue = inputElement.value;
+
+                if (inputValue) setLanguages([...languages, inputValue]);
+
+                inputElement.value = '';
+              }}
+              className='rm-category-btn'
+            >
+              <FaTrash />
+            </Button>
+          </div>
+
+        </Col>
+      </Row>
+    </Container>
   );
 
   function handleDragEnd(event) {
